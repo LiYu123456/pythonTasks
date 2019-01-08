19b9411275fddebdd69ead6de5a396ac21384a3c
lipj <Lipj_java@126.com>
2019-01-08 18:13:23 +0800

    练习2

---
+++ tasks-练习题/002.py
@@ -0,0 +1,9 @@
+# coding:UTF-8
+print("Start......")
+i=int(raw_input("净利润:"))
+arr=[100000,200000,400000,600000,1000000]
+rat=[0.1,0.075,0.05,0.03,0.015,0.01]
+r=0
+for id in range(len(arr)):
+	
+print("End........")
